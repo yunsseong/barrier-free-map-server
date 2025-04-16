4974ef0457977d972cb7719c36490f3ea8852f0e
yunsseong <aster20@hanyang.ac.kr>
2025-04-16 14:49:55 +0900

    spring 초기 설정

---
+++ src/main/java/com/yunsseong/barrier_free_map_server/BarrierFreeMapServerApplication.java
@@ -0,0 +1,13 @@
+package com.yunsseong.barrier_free_map_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarrierFreeMapServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarrierFreeMapServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yunsseong/barrier_free_map_server/BarrierFreeMapServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunsseong.barrier_free_map_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarrierFreeMapServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
