1ca036af39c91f84da6502472c90eab975d1bc5e
yunsseong <aster20@hanyang.ac.kr>
2025-06-01 22:09:59 +0900

    crp 헤더 필터 추가

--- src/main/java/com/yunsseong/barrier_free_map_server/auth/config/SecurityConfig.java
+++ src/main/java/com/yunsseong/barrier_free_map_server/auth/config/SecurityConfig.java
@@ -1,7 +1,13 @@
 package com.yunsseong.barrier_free_map_server.auth.config;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -11,6 +17,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -40,6 +49,26 @@ public class SecurityConfig {
                 );
         return http.build();
     }
+
+    @Bean
+    public FilterRegistrationBean<OncePerRequestFilter> cspHeaderFilter() {
+        FilterRegistrationBean<OncePerRequestFilter> registrationBean = new FilterRegistrationBean<>();
+
+        registrationBean.setFilter(new OncePerRequestFilter() {
+            @Override
+            protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+                    throws ServletException, IOException {
+                // CSP 방식으로 iframe 허용
+                response.setHeader("Content-Security-Policy", "frame-ancestors 'self' https://freepath.hyu.ng");
+                filterChain.doFilter(request, response);
+            }
+        });
+
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return registrationBean;
+    }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
