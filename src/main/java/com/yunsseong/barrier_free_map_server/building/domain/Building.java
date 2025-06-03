ce45fb361fef0d81b61b736eb879d731daddb24b
yunsseong <aster20@hanyang.ac.kr>
2025-06-04 04:33:24 +0900

    층 정렬 순서 정의

--- src/main/java/com/yunsseong/barrier_free_map_server/building/domain/Building.java
+++ src/main/java/com/yunsseong/barrier_free_map_server/building/domain/Building.java
@@ -3,16 +3,7 @@ package com.yunsseong.barrier_free_map_server.building.domain;
 import com.yunsseong.barrier_free_map_server.floor.domain.Floor;
 import com.yunsseong.barrier_free_map_server.map.domain.BarrierFreeMap;
 import com.yunsseong.barrier_free_map_server.common.domain.Coordinate;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Embedded;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -52,6 +43,7 @@ public class Building {
     @Setter
     @Builder.Default
     @OneToMany(mappedBy = "building", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OrderBy("idx ASC")
     private List<Floor> floors = new ArrayList<>();
 
     @Setter
